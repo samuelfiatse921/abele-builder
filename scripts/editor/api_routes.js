ef9b245dbcfa5594fb7aed1273fdcf2bd8cc6c94
samuelfiatse921 <samuelfiatse921@gmail.com>
2025-09-16 09:53:26 +0000

    changed routes to sandbox

--- scripts/editor/api_routes.js
+++ scripts/editor/api_routes.js
@@ -1,6 +1,6 @@
-// var api_endpoint = "https://abele-backend.onrender.com/api/v1" //sandbox
-// var abele_marketplace = "https://abele-marketplace.vercel.app/index.html"; //sandbox
+var api_endpoint = "https://abele-backend.onrender.com/api/v1" //sandbox
+var abele_marketplace = "https://abele-marketplace.vercel.app/index.html"; //sandbox
 
-let api_endpoint = "http://localhost:9000/api/v1" //local
-var abele_marketplace = "http://127.0.0.1:53329/index.html"; //local
+// let api_endpoint = "http://localhost:9000/api/v1" //local
+// var abele_marketplace = "http://127.0.0.1:53329/index.html"; //local
 
